aad952a6e4f1efd231dcbda979b17f892874377f
Arsh Anand <58482036+arshanand1@users.noreply.github.com>
2023-09-14 10:18:03 +0530

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// do not do such conversions
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//undefined sabke saath false hota hai
+
+// === 
+
+console.log("2" === 2);

27496c2442efe962ef13f1ec51bb3418d22ace5d
Arsh Anand <arshanand350@gmail.com>
2023-11-10 21:37:26 +0530

    scope explained

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) //this works 
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) //here we have stored̦ function in a variable and hence it throws error when the variable as a function is ran before the finction declaration
+const addTwo = function(num){
+    return num + 2
+}
+
+//this concept is known as hoiting

ba4824ceab8b70a98c6e14dd32625a41d403184b
Arsh Anand <arshanand350@gmail.com>
2023-11-10 15:10:47 +0530

    Object properties

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,3 +1,55 @@
 //const tinderUser = new Object(); //singleton object
 
 const tinderUser2 = {} //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "c", 6: "d"}
+// const obj3 = {obj1, obj2}
+
+// const answer= Object.assign({}, obj1, obj2, obj3)
+//now using spread function
+const answer = {...obj1, ...obj2, ...obj3}
+console.log(answer);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); 
+//in this keys and values are there
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
